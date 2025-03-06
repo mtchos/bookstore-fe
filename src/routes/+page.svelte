e78373c1043cf19911ad49e1b57e5566efa01759
Matheus Sousa <matheus.sousa@omnisaude.app>
2025-03-06 17:09:59 -0300

    feat: get data from backend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,27 @@
-<script module>
-	let books = [
-		{
-			title: 'Quincas Borba',
-			author: 'Machado de Assis'
-		},
-		{
-			title: 'Olhos d\'Água',
-			author: 'Conceição Evaristo'
-		},
-		{
-			title: 'Moby-Dick',
-			author: 'Herman Melville'
-		}
-	];
-
-</script>
-
 <script lang="ts">
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+
+	type Book = {
+		id: string;
+		isbn: string;
+		name: string;
+		author: string;
+		year: string;
+		publisher: string;
+	}
+
+	let books = $state([] as Book[]);
+
+	onMount(async () => {
+		try {
+			const response = await axios.get("http://localhost:8080/books")
+			books = await response.data;
+		} catch (error) {
+			console.error(error);
+		}
+	})
+
 	let name = 'SvelteKit';
 	let badabing = $state(0);
 </script>
@@ -32,9 +37,15 @@
 		<div>
 			<span>#{index}</span>
 			<br />
-			<span>Author: {book.author}</span>
+			<span>Título: {book.name}</span>
 			<br />
-			<span>Title: {book.title}</span>
+			<span>Autor: {book.author}</span>
+			<br />
+			<span>Ano: {book.year}</span>
+			<br />
+			<span>ISBN: {book.isbn}</span>
+			<br />
+			<span>Editora: {book.publisher}</span>
 			<br />
 			<br />
 		</div>
